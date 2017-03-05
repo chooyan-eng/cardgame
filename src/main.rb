a50b1fa59fd3afe535772db551c55181266d7c87
chooyan-eng <chooyan.engineer@gmail.com>
2017-03-05 23:40:15 +0900

    [add] templete of main class

---
+++ src/main.rb
@@ -0,0 +1,19 @@
+require 'model/player'
+require 'model/field'
+require 'util/data_io'
+
+# check if the command is "init"
+# if "init", delete all the data.
+
+# load present status
+
+# if game has not started yet, ask player if he/she would join the game.
+# and game would start when two players join.
+
+# if game has already started, load current field and player data.
+
+# if it's player's turn, ask player what to do.
+
+# if it's enemy's turn, show message to wait.
+
+# if game has already finished, show result message.

53f4fd483261df283bdb864973ea0450beb59900
chooyan_eng <chooyan.engineer@gmail.com>
2017-03-05 17:50:26 +0900

    [add] tests for player.rb

--- src/model/player.rb
+++ src/model/player.rb
@@ -1,9 +1,12 @@
 module Model
   class Player
-    attr_reader :money, :job, :cards
+    attr_reader :money, :jobs, :cards
 
-    def initialize(money, job, cards)
-      @money = money, @job = job, @cards = cards 
+    def initialize()
+      @money = 0
+      @jobs = []
+      @cards = []
+      @multiple_job = false
     end
 
     def gain_money(amount)
@@ -12,24 +15,33 @@ module Model
 
     def lose_money(amount)
       @money -= amount
+      @money = [@money, 0].max
     end
 
     def gain_job(job_id)
-      @job = job_id
+      @jobs.push(job_id) if @jobs.empty? || job_multiplied? 
     end
 
-    def lose_job()
-      @job = nil
+    def lose_job(job_id)
+      index = @jobs.find_index(job_id)
+      @jobs.delete_at(index) unless index.nil?
     end
 
     def gain_card(card_id)
-      cards = [] if cards.nil?
-      cards.add(card_id)
+      @cards.push(card_id)
     end
 
     def lose_card(card_id)
-      index = cards.find_index(card_id)
-      cards.delete_at(index) unless index.nil?
+      index = @cards.find_index(card_id)
+      @cards.delete_at(index) unless index.nil?
+    end
+
+    def job_multiply
+      @multiple_job = true
+    end
+
+    def job_multiplied?
+      @multiple_job
     end
   end
 end
---
+++ test/model/player_test.rb
@@ -0,0 +1,69 @@
+require 'test/unit'
+require 'model/player'
+
+class TestPlayer < Test::Unit::TestCase
+  def test_has_100_doller_after_gaining_100_doller
+    @player = Model::Player.new 
+    @player.gain_money(100)
+    assert_equal(100, @player.money)
+  end
+
+  def test_money_cannnot_be_less_than_0
+    @player = Model::Player.new 
+    @player.gain_money(100)
+    @player.lose_money(200)
+    assert_equal(0, @player.money)
+  end
+
+  def test_can_gain_a_job
+    @player = Model::Player.new 
+    @player.gain_job(1)
+    assert_equal(1, @player.jobs[0])
+  end
+
+  def test_cannot_gain_a_secondary_job_without_card_effect
+    @player = Model::Player.new 
+    @player.gain_job(1)
+    @player.gain_job(2)
+    assert_equal(nil, @player.jobs[1])
+  end
+
+  def test_can_gain_a_secondary_job_after_card_effect
+    @player = Model::Player.new 
+    @player.gain_job(1)
+    @player.job_multiply
+    @player.gain_job(2)
+    assert_equal(2, @player.jobs[1])
+  end
+
+  def test_lose_a_job
+    @player = Model::Player.new 
+    @player.gain_job(1)
+    @player.lose_job(1)
+    assert_equal([], @player.jobs)
+  end
+
+  def test_lose_first_job
+    @player = Model::Player.new 
+    @player.gain_job(1)
+    @player.job_multiply
+    @player.gain_job(2)
+    @player.lose_job(1)
+    assert_equal(2, @player.jobs[0])
+  end
+
+  def test_gain_a_card
+    @player = Model::Player.new 
+    @player.gain_card(1)
+    @player.gain_card(2)
+    assert_equal(2, @player.cards.size)
+  end
+
+  def test_lose_a_card
+    @player = Model::Player.new 
+    @player.gain_card(1)
+    @player.gain_card(2)
+    @player.lose_card(2)
+    assert_equal(1, @player.cards.size)
+  end
+end
